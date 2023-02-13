c90340d572576dddb66623cd0f9be507f1a45821
thanhthanhagl <tran_diep_thanh_thanh@allgrow-labo.jp>
2023-02-13 18:27:59 +0700

    done PC tablet

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -34,4 +34,20 @@ $(document).ready(function () {
             }
         ]
     });
+    //---------------menu mobile -------//
+    $('.c-header__iconmenu').click(function(){
+        $(this).addClass('is-open');
+        $('.c-navmb').addClass('is-open');
+        $('body').addClass('is-overlay');
+    })
+    $('.c-navmb__close').click(function(){
+        $('.c-header__iconmenu').removeClass('is-open');
+        $('.c-navmb').removeClass('is-open');
+        $('body').removeClass('is-overlay');
+    })
+    $('.c-navmb__item').click(function(){
+        $('.c-header__iconmenu').removeClass('is-open');
+        $('.c-navmb').removeClass('is-open');
+        $('body').removeClass('is-overlay');
+    })
 })
