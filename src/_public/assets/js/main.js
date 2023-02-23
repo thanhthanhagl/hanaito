daaa844e455eaacb1f35c1e3dfab4de4d448ef3c
thanhthanhagl <tran_diep_thanh_thanh@allgrow-labo.jp>
2023-02-23 15:38:10 +0700

    fix new issues

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,20 +1,5 @@
 $(document).ready(function () {
     //header
-
-    var child = $('.c-nav__item a').next();
-    if (child) {
-        child.parent().addClass('is-parent');
-    }
-    $('.is-parent a').click(function () {
-        $('.c-nav__submenu').not($(this).next()).slideUp();
-        $(this).next(".c-nav__submenu").slideToggle();
-        return false;
-    })
-    $(document).on("click", function (event) {
-        if (!$(event.target).closest(".is-parent").length) {
-            $(".c-nav__submenu").slideUp();
-        }
-    });
     //----------slick slider------------
     $('.c-mainvisual__list').slick({
         infinite: true,
@@ -27,19 +12,27 @@ $(document).ready(function () {
         pauseOnHover: false
     });
     //---------------menu mobile -------//
-    $('.c-header__iconmenu').click(function () {
+    $('.c-header__iconmenu').click(function (e) {
+        e.stopPropagation();
         $(this).addClass('is-open');
-        $('.c-navmb').addClass('is-open');
+        $('.c-navmb').addClass('is-open');        
         $('body').addClass('is-overlay');
     })
-    $('.c-navmb__close').click(function () {
+    $('.c-navmb__close').click(function (e) {
+        e.stopPropagation();
         $('.c-header__iconmenu').removeClass('is-open');
         $('.c-navmb').removeClass('is-open');
         $('body').removeClass('is-overlay');
     })
-    $('.c-navmb__item').click(function () {
+    $('.c-navmb__item').click(function (e) {
+        e.stopPropagation();
         $('.c-header__iconmenu').removeClass('is-open');
         $('.c-navmb').removeClass('is-open');
         $('body').removeClass('is-overlay');
     })
+    $('body,html').click(function(){
+        $('.c-header__iconmenu').removeClass('is-open');
+        $('.c-navmb').removeClass('is-open');
+        $('body').removeClass('is-overlay');
+ });
 })
