2e01fb9bb379700070258a5ecba7c85603817d82
thanhthanhagl <tran_diep_thanh_thanh@allgrow-labo.jp>
2023-03-03 17:20:21 +0700

    done page faq

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     $('.c-header__iconmenu').click(function (e) {
         e.stopPropagation();
         $(this).addClass('is-open');
-        $('.c-navmb').addClass('is-open');        
+        $('.c-navmb').addClass('is-open');
         $('body').addClass('is-overlay');
     })
     $('.c-navmb__close').click(function (e) {
@@ -30,9 +30,30 @@ $(document).ready(function () {
         $('.c-navmb').removeClass('is-open');
         $('body').removeClass('is-overlay');
     })
-    $('body,html').click(function(){
+    $('body,html').click(function () {
         $('.c-header__iconmenu').removeClass('is-open');
         $('.c-navmb').removeClass('is-open');
         $('body').removeClass('is-overlay');
- });
+    });
+    //-----------table of content-----------
+    $('.c-icon').click(function () {
+        $(this).toggleClass('is-open');
+        if ($(this).hasClass('is-open')) {
+            $('.c-faq__toc').addClass('is-open');
+        }
+    })
+    $('.c-toc__item a').click(function () {
+        $('.c-icon').removeClass('is-open');
+        $('.c-faq__toc').removeClass('is-open');
+    })
+    $(window).scroll(function () {
+        var scrollDistance = $(window).scrollTop();
+        if (scrollDistance > 700) {
+            $('.c-icon').addClass("is-scroll");
+        }
+        else {
+            $('.c-icon').removeClass("is-scroll is-open");
+            $('.c-faq__toc').removeClass('is-open');
+        }
+    }).scroll();
 })
